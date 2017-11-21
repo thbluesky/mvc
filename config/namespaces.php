7628163e7b58d1176d4af1963babc9f176cd3f77
thbluesky <yyc579992@163.com>
2017-11-21 20:28:52 +0800

    mvc

---
+++ config/namespaces.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+return [
+	'Controller\\'=>'app/controller',
+	'Model\\'=>'app/model',
+	'View\\'=>'app/view'，
+	''
+];
+*/
+//键和值反转；
+return [
+	'app/controller'=>'Controller\\',
+	'app/model'=>'Model\\',
+	'app/view'=>'View\\',
+	'vendor/three/framework/src'=>'Framework\\',
+	'vendor/four/framework/src'=>'Framework\\'
+
+];
